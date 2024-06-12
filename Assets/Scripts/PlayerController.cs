c8ad4f44c5c201e8594ac65e041a124d72adab39
Adam Farahat <farahatadam@gmail.com>
2024-06-12 13:17:50 -0400

    Finish Mouse rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,12 @@ public class PlayerController : MonoBehaviour
     public float mouseSensitivity = 1f;
     private float verticalRotationStore;
     private Vector2 mouseInput;
+    public bool invertLook;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; 
     }
 
     // Update is called once per frame
@@ -21,5 +22,13 @@ public class PlayerController : MonoBehaviour
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y + mouseInput.x,transform.rotation.eulerAngles.z);
+
+        verticalRotationStore += mouseInput.y;
+        verticalRotationStore = Mathf.Clamp(verticalRotationStore, -60f, 60f);
+        
+        if(invertLook)
+            pointOfView.rotation = Quaternion.Euler(verticalRotationStore, pointOfView.rotation.eulerAngles.y,pointOfView.rotation.eulerAngles.z);
+        else
+            pointOfView.rotation = Quaternion.Euler(-verticalRotationStore, pointOfView.rotation.eulerAngles.y, pointOfView.rotation.eulerAngles.z);
     }
 }
