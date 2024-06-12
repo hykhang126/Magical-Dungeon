1bc54ef080ad99b7cdeab5bbe2f41ceb526552cd
Adam Farahat <farahatadam@gmail.com>
2024-06-12 13:32:58 -0400

    Finish X and Y player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,11 @@ public class PlayerController : MonoBehaviour
     public float mouseSensitivity = 1f;
     private float verticalRotationStore;
     private Vector2 mouseInput;
+    
     public bool invertLook;
+
+    public float moveSpeed = 5f;
+    private Vector3 moveDir, movement;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Mouse Rotation
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y + mouseInput.x,transform.rotation.eulerAngles.z);
@@ -30,5 +35,12 @@ public class PlayerController : MonoBehaviour
             pointOfView.rotation = Quaternion.Euler(verticalRotationStore, pointOfView.rotation.eulerAngles.y,pointOfView.rotation.eulerAngles.z);
         else
             pointOfView.rotation = Quaternion.Euler(-verticalRotationStore, pointOfView.rotation.eulerAngles.y, pointOfView.rotation.eulerAngles.z);
+
+        //Player Movement
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized;
+
+        transform.position += movement * moveSpeed * Time.deltaTime;
     }
 }
