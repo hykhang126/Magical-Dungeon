b88056aadd72a3924e4dc3618165bfee4838309c
Adam Farahat <farahatadam@gmail.com>
2024-08-06 13:55:33 -0400

    Connect to Server and add menu music

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,13 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using TMPro;
 
-public class Launcher : MonoBehaviour
+public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+
+    public GameObject MenuButtons;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CloseMenus();
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting to Network...";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    //Function to make sure all menus are disabled
+    void CloseMenus()
+    {
+        loadingScreen.SetActive(false);
+        MenuButtons.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+
+        loadingText.text = "Joining Lobby";
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenus();
+
+        MenuButtons.SetActive(true);
     }
 
     // Update is called once per frame
